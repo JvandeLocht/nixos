bc29d614baa6efddd52eeb1ea2a2f379243e381e
Jan van de Locht <jan@vandelocht.uk>
2024-01-16 20:57:12 +0100

    Added sop-nix.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Jans's NixOS Flake";
 
   nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
+    experimental-features = ["nix-command" "flakes"];
     #    substituters = [
     #      # Replace the official cache with a mirror located in China
     #      "https://mirrors.ustc.edu.cn/nix-channels/store"
@@ -20,7 +20,7 @@
 
   inputs = {
     # Official NixOS package source, using nixos-unstable branch here
-#    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    #    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -32,67 +32,73 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix.url = "github:Mic92/sops-nix";
+    #    nixvim = {
+    #      url = "github:nix-community/nixvim";
+    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    # url = "github:nix-community/nixvim/nixos-23.05";
 
-#    nixvim = {
-#      url = "github:nix-community/nixvim";
-      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      # url = "github:nix-community/nixvim/nixos-23.05";
+    #      inputs.nixpkgs.follows = "nixpkgs";
+    #    };
 
-#      inputs.nixpkgs.follows = "nixpkgs";
-#    };
-
-#    hyprland.url = "github:hyprwm/Hyprland";
-#    hyprgrass = {
-#      url = "github:horriblename/hyprgrass";
-#      inputs.hyprland.follows = "hyprland"; # IMPORTANT
-#    };
+    #    hyprland.url = "github:hyprwm/Hyprland";
+    #    hyprgrass = {
+    #      url = "github:horriblename/hyprgrass";
+    #      inputs.hyprland.follows = "hyprland"; # IMPORTANT
+    #    };
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, ... }@inputs:
-    let
-      inherit (self) outputs;
-      system = "x86_64-linux";
-    in {
-      nixosConfigurations = {
-        "gnome_laptop" = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs;
-          };
-          modules = [
-
-	    # Nix User Repo
-            { nixpkgs.overlays = [ nur.overlay ]; }
-            ({ pkgs, ... }:
-              let
-                nur-no-pkgs = import nur {
-                  nurpkgs = import nixpkgs { system = "x86_64-linux"; };
-                };
-              in {
-                imports = [ nur-no-pkgs.repos.iopq.modules.xraya ];
-                services.xraya.enable = true;
-              })
-
-            # Classic NixOS Configuration
-            ./hosts/gnome_laptop/configuration.nix
-
-            # home-manager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = {
-                  inherit inputs outputs;
-                };
-                users.jan.imports = [
-                  ./hosts/gnome_laptop/home.nix
-#                  nixvim.homeManagerModules.nixvim
-                ];
-              };
-            }
-          ];
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nur,
+    sops-nix,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations = {
+      "gnome_laptop" = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs;
         };
-      };
+        modules = [
+          # Nix User Repo
+          {nixpkgs.overlays = [nur.overlay];}
+          ({pkgs, ...}: let
+            nur-no-pkgs = import nur {
+              nurpkgs = import nixpkgs {system = "x86_64-linux";};
+            };
+          in {
+            imports = [nur-no-pkgs.repos.iopq.modules.xraya];
+            services.xraya.enable = true;
+          })
 
+          # Classic NixOS Configuration
+          ./hosts/gnome_laptop/configuration.nix
+
+          # Secret Managment
+          sops-nix.nixosModules.sops
+
+          # home-manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                inherit inputs outputs;
+              };
+              users.jan.imports = [
+                ./hosts/gnome_laptop/home.nix
+                #                  nixvim.homeManagerModules.nixvim
+              ];
+            };
+          }
+        ];
+      };
     };
+  };
 }
