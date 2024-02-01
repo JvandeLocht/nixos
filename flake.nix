e8964cb63abe9f42d7e429a54919c2b8497b9b26
Jan van de Locht <jan@vandelocht.uk>
2024-02-01 15:16:17 +0100

    Added Cuda.

--- flake.nix
+++ flake.nix
@@ -12,9 +12,11 @@
     extra-substituters = [
       # Nix community's cache server
       "https://nix-community.cachix.org"
+      # "https://app.cachix.org/cache/cuda-maintainers"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
 
