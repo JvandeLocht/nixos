a34e14769efd3fc954b420bf301b3e8030ce6ca2
Jan van de Locht <jan@vandelocht.uk>
2024-02-24 14:33:18 +0100

    Played with some network setups for microvm.nix.

--- nixos/modules/microvm/default.nix
+++ nixos/modules/microvm/default.nix
@@ -1,8 +1,78 @@
 {
   # imports = [./microvm.nix];
-  #
-  # networking.useNetworkd = true;
-  #
+
+  # networking = {
+  #   useNetworkd = true;
+  # bridges = {
+  #   "br0" = {
+  #     interfaces = ["wlp7s0"];
+  #   };
+  # };
+  # bonds = {
+  #   "bond0" = {
+  #     interfaces = ["wlp7s0"];
+  #   };
+  # };
+  # interfaces = {
+  # bond0 = {
+  # useDHCP = true;
+  # };
+  # wlp7s0 = {
+  #   useDHCP = true;
+  # };
+  # };
+  # };
+
+  # systemd = {
+  #   services."systemd-networkd".environment.SYSTEMD_LOG_LEVEL = "debug";
+  #   network = {
+  #     netdevs = {
+  #       "10-bond0" = {
+  #         netdevConfig = {
+  #           Kind = "bond";
+  #           Name = "bond0";
+  #         };
+  #         bondConfig = {
+  #           Mode = "active-backup";
+  #           PrimaryReselectPolicy = "always";
+  #           MIIMonitorSec = "1s";
+  #           TransmitHashPolicy = "layer3+4";
+  #         };
+  #       };
+  #     };
+  #     networks = {
+  #       "30-enp" = {
+  #         matchConfig.Name = "enp*";
+  #         networkConfig = {
+  #           Bond = "bond0";
+  #           PrimarySlave = true;
+  #         };
+  #       };
+  #       "30-wlp" = {
+  #         matchConfig.Name = "wlp*";
+  #         networkConfig.Bond = "bond0";
+  #       };
+  #       "40-bond0" = {
+  #         matchConfig.Name = "bond0";
+  #         linkConfig = {
+  #           RequiredForOnline = "carrier";
+  #         };
+  #         dhcpV6Config = {
+  #           UseDNS = true;
+  #           UseNTP = true;
+  #         };
+  #         ipv6AcceptRAConfig = {
+  #           DHCPv6Client = "always";
+  #         };
+  #         networkConfig = {
+  #           IPv6AcceptRA = true;
+  #           LinkLocalAddressing = "no";
+  #           DHCP = "yes";
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
   # systemd.network = {
   #   enable = true;
   #   netdevs."20-br0" = {
