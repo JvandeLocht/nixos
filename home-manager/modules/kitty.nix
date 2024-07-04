cfb76c8b7c2480eff8389227efa7fd6dceaedfbf
Jan van de Locht <jan@vandelocht.uk>
2024-07-04 21:20:55 +0200

    Added a Option to enable kitty.
    This includes my personal config.

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,21 +1,33 @@
-{pkgs, ...}: {
-  programs.kitty = {
-    enable = true;
-    font = {
-      size = 14;
-      package = pkgs.nerdfonts;
-      name = "MesloLGM Nerd Font Mono";
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    kitty.enable =
+      lib.mkEnableOption "enables kitty";
+  };
+
+  config = lib.mkIf config.kitty.enable {
+    programs.kitty = {
+      enable = true;
+      font = {
+        size = 14;
+        package = pkgs.nerdfonts;
+        name = "MesloLGM Nerd Font Mono";
+      };
+      theme = "One Dark";
+      shellIntegration.enableFishIntegration = true;
+      settings = {
+        scrollback_lines = 10000;
+        enable_audio_bell = false;
+        update_check_interval = 0;
+      };
+      extraConfig = ''
+        hide_window_decorations yes
+        background_opacity 0.8
+      '';
     };
-    theme = "One Dark";
-    shellIntegration.enableFishIntegration = true;
-    settings = {
-      scrollback_lines = 10000;
-      enable_audio_bell = false;
-      update_check_interval = 0;
-    };
-    extraConfig = ''
-      hide_window_decorations yes
-      background_opacity 0.8
-    '';
   };
 }
