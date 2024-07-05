5113b37696be6d16ed9004e00d8a03c88dd63263
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:01:43 +0200

    Made an option for alacritty

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -1,18 +1,28 @@
 {
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 16;
-        # draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-      window = {
-        opacity = 0.9;
-        decorations = "none";
+  lib,
+  config,
+  ...
+}: {
+  options.alacritty = {
+    enable = lib.mkEnableOption "Custom Alacritty configuration";
+  };
+
+  config = lib.mkIf config.alacritty.enable {
+    programs.alacritty = {
+      enable = true;
+      # custom settings
+      settings = {
+        env.TERM = "xterm-256color";
+        font = {
+          size = 16;
+          # draw_bold_text_with_bright_colors = true;
+        };
+        scrolling.multiplier = 5;
+        selection.save_to_clipboard = true;
+        window = {
+          opacity = 0.9;
+          decorations = "none";
+        };
       };
     };
   };
