ce2cfa55a437882806985394fdd126f1a6b5186f
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:03:18 +0200

    Made an option for swaylock

--- home-manager/modules/swaylock.nix
+++ home-manager/modules/swaylock.nix
@@ -1,30 +1,31 @@
-{ pkgs, ... }:
-let
-  vars = {
-    # Variables Used In Flake
-    user = "jan";
-    homeDir = "/home/jan";
-    location = "$HOME/.setup";
-    terminal = "alacritty";
-    editor = "nvim";
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.swaylock = {
+    enable = lib.mkEnableOption "Swaylock with custom configuration";
   };
-in {
-  programs.swaylock = {
-    enable = true;
-    package = pkgs.swaylock-effects;
-    settings = {
-      clock = true;
-      font-size = 24;
-      indicator-idle-visible = false;
-      indicator-radius = 100;
-      line-color = "ffffff";
-      show-failed-attempts = true;
-      image = "${vars.location}/img/nixos_wallpaper.jpg";
-      effect-blur = "7x5";
-      effect-vignette = "0.5:0.5";
-      grace = 2;
-      fade-in = 0.2;
-      daemonize = true;
+
+  config = lib.mkIf config.swaylock.enable {
+    programs.swaylock = {
+      enable = true;
+      package = pkgs.swaylock-effects;
+      settings = {
+        clock = true;
+        font-size = 24;
+        indicator-idle-visible = false;
+        indicator-radius = 100;
+        line-color = "ffffff";
+        show-failed-attempts = true;
+        image = "$HOME/.setup/img/nixos_wallpaper.jpg";
+        effect-blur = "7x5";
+        effect-vignette = "0.5:0.5";
+        grace = 2;
+        fade-in = 0.2;
+        daemonize = true;
+      };
     };
   };
 }
