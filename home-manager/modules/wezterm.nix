9c5ff4333a18aaccd9a010731462761a2939e0be
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:03:27 +0200

    Made an option for wezterm

--- home-manager/modules/wezterm.nix
+++ home-manager/modules/wezterm.nix
@@ -1,30 +1,39 @@
 {
-  programs.wezterm = {
-    enable = true;
-    enableBashIntegration = true;
-    extraConfig =
-      /*
-      lua
-      */
-      ''
-        -- Pull in the wezterm API
-        local wezterm = require 'wezterm'
+  lib,
+  config,
+  ...
+}: {
+  options.wezterm = {
+    enable = lib.mkEnableOption "WezTerm terminal emulator";
+  };
 
-        -- This will hold the configuration.
-        local config = wezterm.config_builder()
+  config = lib.mkIf config.wezterm.enable {
+    programs.wezterm = {
+      enable = true;
+      enableBashIntegration = true;
+      extraConfig =
+        /*
+        lua
+        */
+        ''
+          -- Pull in the wezterm API
+          local wezterm = require 'wezterm'
 
-        -- This is where you actually apply your config choices
+          -- This will hold the configuration.
+          local config = wezterm.config_builder()
 
-        -- For example, changing the color scheme:
-        config.color_scheme = 'One Dark (Gogh)'
-        -- config.window_decorations = "NONE"
-        config.hide_tab_bar_if_only_one_tab = true
-        config.window_background_opacity = 0.8
-        config.font_size = 14
+          -- This is where you actually apply your config choices
 
+          -- For example, changing the color scheme:
+          config.color_scheme = 'One Dark (Gogh)'
+          -- config.window_decorations = "NONE"
+          config.hide_tab_bar_if_only_one_tab = true
+          config.window_background_opacity = 0.8
+          config.font_size = 14
 
-        -- and finally, return the configuration to wezterm
-        return config
-      '';
+          -- and finally, return the configuration to wezterm
+          return config
+        '';
+    };
   };
 }
