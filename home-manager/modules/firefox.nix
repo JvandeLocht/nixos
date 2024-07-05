0ff50103004a8db04df840f718ab8e0afed07a84
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:02:36 +0200

    Made an option for firefox

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -1,14 +1,25 @@
-{pkgs, ...}: {
-  programs.firefox = {
-    enable = true;
-    package = pkgs.librewolf;
-    profiles = {
-      "main" = {
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-          proton-pass
-          i-dont-care-about-cookies
-        ];
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.firefox = {
+    enable = lib.mkEnableOption "Custom LibreWolf configuration";
+  };
+
+  config = lib.mkIf config.firefox.enable {
+    programs.firefox = {
+      enable = true;
+      package = pkgs.librewolf;
+      profiles = {
+        "main" = {
+          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+            ublock-origin
+            proton-pass
+            i-dont-care-about-cookies
+          ];
+        };
       };
     };
   };
