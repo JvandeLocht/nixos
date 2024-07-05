984e068ebdb01ac23ff5d2f3f20f753527c31160
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:03:07 +0200

    made an option for swayidle

--- home-manager/modules/swayidle.nix
+++ home-manager/modules/swayidle.nix
@@ -1,16 +1,27 @@
-{ pkgs, ... }: {
-  services.swayidle = {
-    enable = true;
-    systemdTarget = "hyprland-session.target";
-    timeouts = [
-      {
-        timeout = 300;
-        command = "${pkgs.swaylock-effects}/bin/swaylock";
-      }
-      {
-        timeout = 330;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-      }
-    ];
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.swayidle = {
+    enable = lib.mkEnableOption "Swayidle with custom configuration";
+  };
+
+  config = lib.mkIf config.swayidle.enable {
+    services.swayidle = {
+      enable = true;
+      systemdTarget = "hyprland-session.target";
+      timeouts = [
+        {
+          timeout = 300;
+          command = "${pkgs.swaylock-effects}/bin/swaylock";
+        }
+        {
+          timeout = 330;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+    };
   };
 }
