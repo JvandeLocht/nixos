78bc47d90d15fa29af53732b4c9c882ea48b8788
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:01:54 +0200

    Made an option for bash

--- home-manager/modules/bash.nix
+++ home-manager/modules/bash.nix
@@ -1,19 +1,33 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    upower
-    nix-output-monitor
-  ];
-  programs.zellij.enableBashIntegration = true;
-  programs.bash = {
-    initExtra = ''eval "$(starship init bash)"'';
-    shellAliases = {
-      p = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
-      ng = "sudo nixos-rebuild switch --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
-      ngt = "sudo nixos-rebuild test --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
-      j = "z";
-      t = "zellij";
-      sj = "ssh jan@192.168.178.40";
-      sa = "ssh ae@192.168.178.40";
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.bash = {
+    enable = lib.mkEnableOption "Custom Bash configuration";
+  };
+
+  config = lib.mkIf config.bash.enable {
+    home.packages = with pkgs; [
+      upower
+      nix-output-monitor
+    ];
+
+    programs.zellij.enableBashIntegration = true;
+
+    programs.bash = {
+      enable = true;
+      initExtra = ''eval "$(starship init bash)"'';
+      shellAliases = {
+        p = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
+        ng = "sudo nixos-rebuild switch --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
+        ngt = "sudo nixos-rebuild test --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
+        j = "z";
+        t = "zellij";
+        sj = "ssh jan@192.168.178.40";
+        sa = "ssh ae@192.168.178.40";
+      };
     };
   };
 }
