601d5b782f3c4f76eaacf8b4c7adcaa156cb2875
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:02:44 +0200

    made an option for fish

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -1,31 +1,42 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    fishPlugins.z
-    grc # for fish
-    upower
-    nix-output-monitor
-  ];
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.fish = {
+    enable = lib.mkEnableOption "Custom Fish shell configuration";
+  };
 
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-    '';
-    plugins = [
-      # Enable a plugin (here grc for colorized command output) from nixpkgs
-      {
-        name = "grc";
-        src = pkgs.fishPlugins.grc.src;
-      }
+  config = lib.mkIf config.fish.enable {
+    home.packages = with pkgs; [
+      fishPlugins.z
+      grc # for fish
+      upower
+      nix-output-monitor
     ];
-    shellAliases = {
-      p = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
-      ng = "sudo nixos-rebuild switch --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
-      ngt = "sudo nixos-rebuild test --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
-      j = "z";
-      k = "kubectl";
-      t = "zellij";
-      st = "ssh admin@192.168.178.40";
+
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      plugins = [
+        # Enable a plugin (here grc for colorized command output) from nixpkgs
+        {
+          name = "grc";
+          src = pkgs.fishPlugins.grc.src;
+        }
+      ];
+      shellAliases = {
+        p = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
+        ng = "sudo nixos-rebuild switch --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
+        ngt = "sudo nixos-rebuild test --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
+        j = "z";
+        k = "kubectl";
+        t = "zellij";
+        st = "ssh admin@192.168.178.40";
+      };
     };
   };
 }
