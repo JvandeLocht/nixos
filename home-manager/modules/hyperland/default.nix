0dfd4c37c0df654504c2babfbca4e271125acd1c
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:00:22 +0200

    Made hyprland option

--- home-manager/modules/hyperland/default.nix
+++ home-manager/modules/hyperland/default.nix
@@ -1,29 +1,40 @@
-{ config, pkgs, inputs, ... }: {
-
-  imports = [ ./config.nix ./hyprpaper.nix ];
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    # plugins = [ inputs.hyprgrass.packages.${pkgs.system}.default ];
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  options.hyprlandConfig = {
+    enable = lib.mkEnableOption "Custom Hyprland configuration";
   };
 
-  home.packages = (with pkgs; [
-    libnotify
-    mpd
-    gnome.gnome-keyring
-    gnome.seahorse
-    libgnome-keyring
-    libsecret
-    wl-clipboard
-    hyprpaper
-    pavucontrol
-    grimblast
-    blueberry
-    swaynotificationcenter
-    wlogout
-    (callPackage ../../../pkgs/iio-hyprland.nix { })
-    wvkbd # On screen keyboard
-    fractal # Matrix client
-  ]);
+  config = lib.mkIf config.hyprlandConfig.enable {
+    imports = [./config.nix ./hyprpaper.nix];
+
+    wayland.windowManager.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+      # plugins = [ inputs.hyprgrass.packages.${pkgs.system}.default ];
+    };
+
+    home.packages = with pkgs; [
+      libnotify
+      mpd
+      gnome.gnome-keyring
+      gnome.seahorse
+      libgnome-keyring
+      libsecret
+      wl-clipboard
+      hyprpaper
+      pavucontrol
+      grimblast
+      blueberry
+      swaynotificationcenter
+      wlogout
+      (callPackage ../../../pkgs/iio-hyprland.nix {})
+      wvkbd # On screen keyboard
+      fractal # Matrix client
+    ];
+  };
 }
