53bad8c5db39e84fb3cba4d4f9b6b1a8cbbeb632
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:02:55 +0200

    made an option for gtk.

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -1,26 +1,36 @@
-{ pkgs, ... }: {
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Amber";
-    size = 20;
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.gtkThemes = {
+    enable = lib.mkEnableOption "Custom pointer cursor and GTK theme configuration";
   };
 
-  gtk = {
-    enable = true;
-    theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
+  config = lib.mkIf config.gtkThemes.enable {
+    home.pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Amber";
+      size = 20;
     };
-    iconTheme = {
-      package = pkgs.libsForQt5.breeze-icons;
-      name = "breeze-dark";
-    };
-    font = {
-      name = "Sans";
-      size = 11;
+
+    gtk = {
+      enable = true;
+      theme = {
+        package = pkgs.flat-remix-gtk;
+        name = "Flat-Remix-GTK-Grey-Darkest";
+      };
+      iconTheme = {
+        package = pkgs.libsForQt5.breeze-icons;
+        name = "breeze-dark";
+      };
+      font = {
+        name = "Sans";
+        size = 11;
+      };
     };
   };
-
 }
