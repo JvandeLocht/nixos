eafe035d0f7b42a43923b5d18216338765e47754
Jan van de Locht <jan@vandelocht.uk>
2024-07-05 22:03:37 +0200

    Made an option for zsh

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -1,36 +1,58 @@
 {
-  programs = {
-    starship.enable = true;
-    autojump = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      # initExtra = "zellij";
-      shellAliases = {
-        p = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
-        ng = "sudo nixos-rebuild switch --flake ~/.setup#gnome_laptop --log-format internal-json -v |& nom --json";
-        ngt = "sudo nixos-rebuild test --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
-        # j = "z";
-        # k = "kubectl";
-        t = "zellij";
-        sp = "ssh root@192.168.178.40";
-      };
-      oh-my-zsh = {
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options = {
+    zsh.enable =
+      lib.mkEnableOption "enables preconfigured zsh";
+  };
+
+  config = lib.mkIf config.zsh.enable {
+    home.packages = with pkgs; [
+      upower
+      nix-output-monitor
+    ];
+    programs = {
+      starship.enable = true;
+      autojump = {
         enable = true;
-        theme = "robbyrussell";
-        plugins = [
-          "git"
-          "npm"
-          "history"
-          "node"
-          "rust"
-          "fluxcd"
-          "kubectl"
-        ];
+        enableZshIntegration = true;
+      };
+      zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        initExtra = ''
+          if [[ -z "$ZELLIJ" ]]; then
+            if command -v zellij >/dev/null 2>&1; then
+              zellij attach -c
+            fi
+          fi
+        '';
+        shellAliases = {
+          p = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
+          ng = "sudo nixos-rebuild switch --flake ~/.setup#gnome_laptop --log-format internal-json -v |& nom --json";
+          ngt = "sudo nixos-rebuild test --log-format internal-json -v --flake ~/.setup#gnome_laptop &| nom --json";
+          # j = "z";
+          # k = "kubectl";
+          t = "zellij";
+          sp = "ssh root@192.168.178.40";
+        };
+        oh-my-zsh = {
+          enable = true;
+          theme = "robbyrussell";
+          plugins = [
+            "git"
+            "npm"
+            "history"
+            "node"
+            "rust"
+            "fluxcd"
+            "kubectl"
+          ];
+        };
       };
     };
   };
