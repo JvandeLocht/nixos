3ca48d3d4e06254e7b25ebc6cf57f006ecb4cfda
Jan van de Locht <jan@vandelocht.uk>
2024-08-11 17:43:21 +0200

    Switched to nvidia open

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,8 +1,7 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
+{ pkgs
+, lib
+, config
+, ...
 }: {
   options.nvidia = {
     enable = lib.mkEnableOption "Set up NVIDIA and OpenGL";
@@ -20,7 +19,7 @@
     # cudnn
     # ];
     # Load nvidia driver for Xorg and Wayland
-    services.xserver.videoDrivers = ["nvidia"];
+    services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.nvidia = {
       # Modesetting is required.
@@ -39,7 +38,7 @@
       # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
       # Only available from driver 515.43.04+
       # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-      open = false;
+      open = true;
 
       # Enable the Nvidia settings menu,
       # accessible via `nvidia-settings`.
