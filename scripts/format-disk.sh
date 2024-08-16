8aab4c1e596fc3a3b38c03a18b0793538bf500ab
Jvandelocht <jan@vandelocht.uk>
2024-08-16 06:30:54 +0000

    uncommented zfs stuff and nixos config stuff.

--- scripts/format-disk.sh
+++ scripts/format-disk.sh
@@ -2,8 +2,7 @@
 
 # Prompt user for necessary variables
 # read -p "Enter your SSH key: " SSH_KEY
-# read -p "Enter your host ID: " HOST_ID
-# read -p "Enter your username: " USERNAME
+SSH_KEY=""
 #
 # # Add SSH key (optional)
 # mkdir -p /home/nixos/.ssh
@@ -48,79 +47,95 @@ else
   parted $DISK mkpart primary 512MB $PART_SIZE
   parted $DISK mkpart primary linux-swap $PART_SIZE 100%
   parted $DISK mkpart ESP fat32 1MB 512MB
-  parted $DISK set 3 esp on
-  mkfs.fat -F32 -n boot ${DISK}3
+  read -p "Specify the swap partition (should be 3 or p3): " BOOT
+  parted $DISK set ${BOOT} esp on
+  mkfs.fat -F32 -n boot ${DISK}${BOOT}
 fi
 
 lsblk
 
 echo "-----"
 #
-# Initialize swap
-# mkswap -L swap ${DISK}2
-#
-# # Create ZFS root pool
-# zpool create -f rpool ${DISK}1
-# zfs set compression=on rpool
-#
-# # Create ZFS datasets and mount them
-# zfs create -p -o mountpoint=legacy rpool/eyd/root
-# zfs set xattr=sa rpool/eyd/root
-# zfs set acltype=posixacl rpool/eyd/root
-# zfs snapshot rpool/eyd/root@blank
-# zfs create -p -o mountpoint=legacy rpool/eyd/nix
-# zfs create -p -o mountpoint=legacy rpool/eyd/per
-# mkdir -p /mnt
-# mount -t zfs rpool/eyd/root /mnt
-# mkdir -p /mnt/{nix,boot,per}
-# mount -t zfs rpool/eyd/nix /mnt/nix
-# mount -t zfs rpool/eyd/per /mnt/per
-#
-# if [ "$BOOT_TYPE" == "BIOS" ]; then
-  # zfs create -p -o mountpoint=legacy rpool/eyd/boot
-  # mount -t zfs rpool/eyd/boot /mnt/boot
-# else
-  # mount ${DISK}3 /mnt/boot
-# fi
-#
-# # Generate NixOS configuration file
-# nixos-generate-config --root /mnt
-#
-# # Edit configuration.nix
-# cat <<EOF > /mnt/etc/nixos/configuration.nix
-# ## Boot stuff ##
-# boot.supportedFilesystems = [ "zfs" ];
-# boot.loader.grub.device = "${DISK}";
-#
-# # Might be required if running on a virtual machine
-# boot.zfs.devNodes = "/dev/disk/by-path";
-#
-# # Required by zfs. generate with 'head -c4 /dev/urandom | od -t x4 | cut -c9-16'
-# networking.hostId = "$HOST_ID";
-#
-# boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
-#
-# ## User and SSH stuff ##
-# services.openssh.enable = true;
-#
-# security.sudo.wheelNeedsPassword = false;
-# users.users."$USERNAME" = {
-# isNormalUser = true;
-# password = "password";  # Change this once your computer is set up!
-#   home = "/home/$USERNAME";
-#     extraGroups = [ "wheel" "networkmanager" ];
-#       openssh.authorizedKeys.keys = [ "$SSH_KEY" ];
-#       };
-#
-#       ## Packages ##
-#       environment.systemPackages = with pkgs; [ vim wirelesstools git ];
-#
-#       # Enable nmcli
-#       networking.wireless.enable = false;
-#       networking.networkmanager.enable = true;
-#       networking.useDHCP = false;
-#       networking.networkmanager.wifi.scanRandMacAddress = false;
-#       EOF
+echo "Initialize swap"
+echo ""
+read -p "Specify the swap partition (should be 2 or p2): " SWAP
+echo ""
+mkswap -L swap ${DISK}${SWAP}
+
+echo "-----"
+echo "Creating ZFS root pool"
+read -p "Specify the root partition (should be 1 or p1): " ROOT
+echo ""
+zpool create -f rpool ${DISK}${ROOT}
+zfs set compression=on rpool
+
+echo "-----"
+echo "Creating ZFS datasets and mount them"
+zfs create -p -o mountpoint=legacy rpool/eyd/root
+zfs set xattr=sa rpool/eyd/root
+zfs set acltype=posixacl rpool/eyd/root
+zfs snapshot rpool/eyd/root@blank
+zfs create -p -o mountpoint=legacy rpool/eyd/nix
+zfs create -p -o mountpoint=legacy rpool/eyd/per
+mkdir -p /mnt
+mount -t zfs rpool/eyd/root /mnt
+mkdir -p /mnt/{nix,boot,per}
+mount -t zfs rpool/eyd/nix /mnt/nix
+mount -t zfs rpool/eyd/per /mnt/per
+
+if [ "$BOOT_TYPE" == "BIOS" ]; then
+  zfs create -p -o mountpoint=legacy rpool/eyd/boot
+  mount -t zfs rpool/eyd/boot /mnt/boot
+else
+  mount ${DISK}3 /mnt/boot
+fi
+
+echo "-----"
+echo "Generate NixOS configuration file"
+nixos-generate-config --root /mnt
+
+echo "-----"
+echo "Edit configuration.nix"
+# read -p "Enter your host ID: " HOST_ID
+read -p "Enter your username: " USERNAME
+HOST_ID=head -c4 /dev/urandom | od -t x4 | cut -c9-16
+
+cat <<EOF > /mnt/etc/nixos/configuration.nix
+## Boot stuff ##
+boot.supportedFilesystems = [ "zfs" ];
+boot.loader.grub.device = "${DISK}";
+
+# Might be required if running on a virtual machine
+boot.zfs.devNodes = "/dev/disk/by-path";
+
+# Required by zfs. generate with 'head -c4 /dev/urandom | od -t x4 | cut -c9-16'
+networking.hostId = "$HOST_ID";
+
+boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
+
+## User and SSH stuff ##
+services.openssh.enable = true;
+
+security.sudo.wheelNeedsPassword = false;
+users.users."$USERNAME" = {
+isNormalUser = true;
+password = "password";  # Change this once your computer is set up!
+home = "/home/$USERNAME";
+extraGroups = [ "wheel" "networkmanager" ];
+# openssh.authorizedKeys.keys = [ "$SSH_KEY" ];
+};
+
+## Packages ##
+environment.systemPackages = with pkgs; [ vim wirelesstools git ];
+
+# Enable nmcli
+networking.wireless.enable = false;
+networking.networkmanager.enable = true;
+networking.useDHCP = false;
+networking.networkmanager.wifi.scanRandMacAddress = false;
+EOF
+
+
 #
 #       # Perform NixOS installation
 #       nixos-install --verbose --no-root-password
