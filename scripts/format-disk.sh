cdb3dbfedca572bdb0e831f4490ad615df5a3644
Jan van de Locht <jan@vandelocht.uk>
2024-08-16 13:54:29 +0200

    modified patchfile
    modified patch.
    
    modified patch.

--- scripts/format-disk.sh
+++ scripts/format-disk.sh
@@ -84,10 +84,12 @@ mount -t zfs rpool/eyd/nix /mnt/nix
 mount -t zfs rpool/eyd/per /mnt/per
 
 if [ "$BOOT_TYPE" == "BIOS" ]; then
+echo "-----"
   zfs create -p -o mountpoint=legacy rpool/eyd/boot
   mount -t zfs rpool/eyd/boot /mnt/boot
 else
-  mount ${DISK}3 /mnt/boot
+echo "-----"
+  mount ${DISK}${BOOT} /mnt/boot
 fi
 
 echo "-----"
@@ -97,51 +99,106 @@ nixos-generate-config --root /mnt
 echo "-----"
 echo "Edit configuration.nix"
 read -p "Enter your username: " USERNAME
-# Prompt for host ID with an option to generate one
-read -p "Enter your host ID (or type 'generate' to create a random one): " HOST_ID_INPUT
+read -p "Enter your host ID (or leave empty to create a random one): " HOST_ID_INPUT
 
-if [ "$HOST_ID_INPUT" == "generate" ]; then
+if [ "$HOST_ID_INPUT" == "" ]; then
   HOST_ID=$(head -c4 /dev/urandom | od -t x4 | awk '{print $2}')
     echo "Generated host ID: $HOST_ID"
     else
       HOST_ID=$HOST_ID_INPUT
       fi
 
-cat <<EOF > /mnt/etc/nixos/configuration.nix
-## Boot stuff ##
-boot.supportedFilesystems = [ "zfs" ];
-boot.loader.grub.device = "${DISK}";
-
-# Might be required if running on a virtual machine
-boot.zfs.devNodes = "/dev/disk/by-path";
-
-# Required by zfs. generate with 'head -c4 /dev/urandom | od -t x4 | cut -c9-16'
-networking.hostId = "$HOST_ID";
-
-boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
-
-## User and SSH stuff ##
-services.openssh.enable = true;
-
-security.sudo.wheelNeedsPassword = false;
-users.users."$USERNAME" = {
-isNormalUser = true;
-password = "password";  # Change this once your computer is set up!
-home = "/home/$USERNAME";
-extraGroups = [ "wheel" "networkmanager" ];
-# openssh.authorizedKeys.keys = [ "$SSH_KEY" ];
-};
-
-## Packages ##
-environment.systemPackages = with pkgs; [ vim wirelesstools git ];
-
-# Enable nmcli
-networking.wireless.enable = false;
-networking.networkmanager.enable = true;
-networking.useDHCP = false;
-networking.networkmanager.wifi.scanRandMacAddress = false;
+cat <<EOF > /mnt/etc/nixos/configuration.patch
+--- configuration.nix	2024-08-16 12:08:39.856491846 +0000
++++ /mnt/etc/nixos/configuration.nix	2024-08-16 12:21:37.473561228 +0000
+@@ -12,6 +12,10 @@
+ 
+   # Use the GRUB 2 boot loader.
+   boot.loader.grub.enable = true;
++  boot.supportedFilesystems = [ "zfs" ];
++  boot.loader.grub.device = "${DISK}${ROOT}";  # replace  with the actual disk name, eg /dev/nvme0n1
++  boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
++
+   # boot.loader.grub.efiSupport = true;
+   # boot.loader.grub.efiInstallAsRemovable = true;
+   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+@@ -19,12 +23,13 @@
+   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+ 
+   # networking.hostName = "nixos"; # Define your hostname.
++  networking.hostId = "$HOST_ID";
+   # Pick only one of the below networking options.
+   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
++  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+ 
+   # Set your time zone.
+-  # time.timeZone = "Europe/Amsterdam";
++  time.timeZone = "Europe/Amsterdam";
+ 
+   # Configure network proxy if necessary
+   # networking.proxy.default = "http://user:password@proxy:port/";
+@@ -74,13 +79,53 @@
+   #     tree
+   #   ];
+   # };
++  services.openssh.enable = true;  # If using VPS
++
++  security.sudo.wheelNeedsPassword = false;
++  users.users."$USERNAME" = {
++    isNormalUser = true;
++      password = "password";  # Change this once your computer is set up!
++        home = "/home/$USERNAME";
++          extraGroups = [ "wheel" "networkmanager" ];
++            openssh.authorizedKeys.keys = [ "<your ssh key>" ];  # If using VPS
++            };
++    
++  nix.settings = {
++      experimental-features = ["nix-command" "flakes"];
++          trusted-users = ["jan"]; # Add your own username to the trusted list
++            };
++    # Select internationalisation properties.
++    i18n.defaultLocale = "de_DE.UTF-8";
++
++    i18n.extraLocaleSettings = {
++      LC_ADDRESS = "de_DE.UTF-8";
++      LC_IDENTIFICATION = "de_DE.UTF-8";
++      LC_MEASUREMENT = "de_DE.UTF-8";
++      LC_MONETARY = "de_DE.UTF-8";
++      LC_NAME = "de_DE.UTF-8";
++      LC_NUMERIC = "de_DE.UTF-8";
++      LC_PAPER = "de_DE.UTF-8";
++      LC_TELEPHONE = "de_DE.UTF-8";
++      LC_TIME = "de_DE.UTF-8";
++    };
++
++    # Configure keymap in X11
++    services.xserver.xkb = {
++      layout = "de";
++      variant = "";
++    };
++
++    # Configure console keymap
++    console.keyMap = "de";
++
+ 
+   # List packages installed in system profile. To search, run:
+   # $ nix search wget
+-  # environment.systemPackages = with pkgs; [
+-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+-  #   wget
+-  # ];
++  environment.systemPackages = with pkgs; [
++    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
++    wget
++    git
++  ];
+ 
+   # Some programs need SUID wrappers, can be configured further or are
+   # started in user sessions.
 EOF
 
+patch /mnt/etc/nixos/configuration.nix < /mnt/etc/nixos/configuration.patch
 
 echo "Performing NixOS installation"
 nixos-install --verbose --no-root-password
