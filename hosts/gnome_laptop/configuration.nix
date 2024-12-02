3bbf6d8de84fd8155137a429af4764d0e5098e6b
Jan van de Locht <jan@vandelocht.uk>
2024-12-02 09:10:58 +0100

    gnome_laptop: moved systemstate version.

--- hosts/gnome_laptop/configuration.nix
+++ hosts/gnome_laptop/configuration.nix
@@ -108,4 +108,11 @@
   programs.dconf.enable = true;
   nixpkgs.config.permittedInsecurePackages = [ "electron-24.8.6" "electron-22.3.27" "electron-25.9.0" "electron-27.3.11" ];
   hardware.bluetooth.enable = true;
+  # This value determines the NixOS rele se from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
