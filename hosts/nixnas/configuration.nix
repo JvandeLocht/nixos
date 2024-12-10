3c28a64fa336ebcaffc03ab16bdf8175beb012cc
Jan van de Locht <jan@vandelocht.uk>
2024-12-10 10:24:53 +0100

    removed duplicates

--- hosts/nixnas/configuration.nix
+++ hosts/nixnas/configuration.nix
@@ -57,10 +57,6 @@ in
         ExecStart = "${pkgs.zfs}/bin/zpool import tank";
       };
     };
-    # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-    "getty@tty1".enable = false;
-    "autovt@tty1".enable = false;
-
   };
 
   boot = {
--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -17,14 +17,6 @@
 
     };
 
-    systemd.services = {
-      # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-      "getty@tty1".enable = false;
-      "autovt@tty1".enable = false;
-
-    };
-
-
     # Enable the GNOME Desktop Environment.
     services.xserver = {
       displayManager.gdm.enable = true;
