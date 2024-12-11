d0f574a68e921d5b099f23a9f16693af240dac6e
Jan van de Locht <jan@vandelocht.uk>
2024-12-11 13:50:09 +0100

    Modified hypridle

--- home-manager/modules/hypridle.nix
+++ home-manager/modules/hypridle.nix
@@ -12,20 +12,48 @@
       enable = true;
       settings = {
         general = {
-          after_sleep_cmd = "hyprctl dispatch dpms on";
-          ignore_dbus_inhibit = false;
-          lock_cmd = "hyprlock";
+          after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+          lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
+          before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
         };
 
-        listener = [
+        listeners = [
+          # Monitor backlight control
           {
-            timeout = 900;
-            on-timeout = "hyprlock";
+            timeout = 150; # 2.5 minutes
+            # Set monitor backlight to minimum (10), avoiding 0 on OLED monitors
+            onTimeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 1";
+            # Restore monitor backlight
+            onResume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
           }
+          # Keyboard backlight control
+          # Comment out this section if you don't have a keyboard backlight
           {
-            timeout = 1200;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
+            timeout = 150; # 2.5 minutes
+            # Turn off keyboard backlight
+            onTimeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd asus::kbd_backlight set 0";
+            # Turn on keyboard backlight
+            onResume = "${pkgs.brightnessctl}/bin/brightnessctl -rd asus::kbd_backlight";
+          }
+          # Screen locking
+          {
+            timeout = 300; # 5 minutes
+            # Lock screen when timeout has passed
+            onTimeout = "${pkgs.systemd}/bin/loginctl lock-session";
+          }
+          # Display power management
+          {
+            timeout = 330; # 5.5 minutes
+            # Turn off screen when timeout has passed
+            onTimeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+            # Turn on screen when activity is detected after timeout has fired
+            onResume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          }
+          # System suspension
+          {
+            timeout = 1800; # 30 minutes
+            # Suspend PC
+            onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
           }
         ];
       };
