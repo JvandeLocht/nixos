72a0353f3359b0be370f863428aa06f72960d395
Jan van de Locht <jan@vandelocht.uk>
2024-12-31 20:17:08 +0100

    enabled tearing

--- home-manager/modules/hyperland/config.nix
+++ home-manager/modules/hyperland/config.nix
@@ -64,7 +64,7 @@
         col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
         col.inactive_border = rgba(595959aa)
         resize_on_border = true
-
+        allow_tearing = true
         layout = dwindle
     }
 
