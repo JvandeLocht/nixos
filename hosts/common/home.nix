aff99700a4484d02d2e7fe8431840fc58fb4b051
Jan van de Locht <jan@vandelocht.uk>
2025-03-21 22:11:09 +0100

    refractored home.nix

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  pkgs,
-  osConfig,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ../../home-manager/modules
   ];
@@ -19,66 +14,65 @@
     userEmail = "jan@vandelocht.uk";
   };
 
-  home.file = {
-    ".nixos_wallpaper.jpg" = {
-      source = ../../img/nixos_wallpaper.jpg;
-      recursive = true;
+  home = {
+    file = {
+      ".nixos_wallpaper.jpg" = {
+        source = ../../img/nixos_wallpaper.jpg;
+        recursive = true;
+      };
     };
+    packages = with pkgs; [
+      # archives
+      zip
+      xz
+      unzip
+      p7zip
+
+      # CLI
+      typst
+      pandoc
+      yq-go
+      kubeconform
+      kustomize
+      talosctl
+      kubectl
+      kubernetes-helm
+      k9s
+      fluxcd
+      kompose
+      # nixvim
+      nvf
+      minio-client
+      velero
+
+      sops
+      age
+
+      abduco # Allows programs to be run independently from its controlling terminal
+      neofetch
+      lazygit
+      evtest # test Input Events (for example LidSwitch)
+      usbutils
+      android-tools
+      auto-cpufreq
+      sshfs
+      tree # Display filetree
+      htop
+      tldr
+      killall
+    ];
+    # This value determines the home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update home Manager without changing this value. See
+    # the home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "23.05";
   };
 
-  # Packages that should be installed to the user profile.
-  home.packages = with pkgs; [
-    # archives
-    zip
-    xz
-    unzip
-    p7zip
-
-    # CLI
-    typst
-    pandoc
-    yq-go
-    kubeconform
-    kustomize
-    talosctl
-    kubectl
-    kubernetes-helm
-    k9s
-    fluxcd
-    kompose
-    # nixvim
-    nvf
-    minio-client
-    velero
-
-    sops
-    age
-
-    abduco # Allows programs to be run independently from its controlling terminal
-    neofetch
-    lazygit
-    evtest # test Input Events (for example LidSwitch)
-    usbutils
-    android-tools
-    auto-cpufreq
-    sshfs
-    tree # Display filetree
-    htop
-    tldr
-    killall
-  ];
-
   services.syncthing.enable = true;
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.05";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
