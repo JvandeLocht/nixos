a47aa6066814d5e0a5521b14e7bc4bbcb75e9900
Jan van de Locht <jan@vandelocht.uk>
2025-08-03 01:40:47 +0200

    enabled nvidia powermanagement

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -3,7 +3,8 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   options.nvidia = {
     enable = lib.mkEnableOption "Set up NVIDIA and OpenGL";
   };
@@ -19,14 +20,14 @@
     # cudnn
     # ];
     # Load nvidia driver for Xorg and Wayland
-    services.xserver.videoDrivers = ["nvidia"];
+    services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.nvidia = {
       # Modesetting is required.
       modesetting.enable = true;
 
       # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      powerManagement.enable = false;
+      powerManagement.enable = true;
       # Fine-grained power management. Turns off GPU when not in use.
       # Experimental and only works on modern Nvidia GPUs (Turing or newer).
       powerManagement.finegrained = false;
