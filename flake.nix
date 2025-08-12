91e02701d17815007d78e01d869c6390983682ad
Jan van de Locht <jan@vandelocht.uk>
2025-08-12 23:18:22 +0200

    Updated nixpkgs stable to 25.05

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-wsl = {
