c06f486ace313fc65dc7c0398138339a831bf816
Jan van de Locht <jan@vandelocht.uk>
2025-08-12 22:18:07 +0200

    Added necessary opt-ins to persistence for netbird

--- hosts/nixnas/opt-in.nix
+++ hosts/nixnas/opt-in.nix
@@ -5,6 +5,8 @@
       "/root/.local/share/nix"
       "/var/log"
       "/var/lib/nixos"
+      "/var/lib/netbird"
+      "/var/lib/netbird-wt0"
       "/var/lib/libvirt"
       "/var/cache/libvirt"
       "/var/lib/containers/storage"
@@ -15,6 +17,8 @@
       "/etc/NetworkManager/system-connections"
       "/etc/ssh"
       "/etc/samba"
+      "/etc/netbird"
+      "/etc/netbird-wt0"
       "/var/lib/samba"
       "/var/lib/cups"
       "/var/spool/cups"
@@ -25,15 +29,15 @@
         mode = "u=rwx,g=rx,o=";
       }
     ];
-#    files = [
-#      "/etc/machine-id"
-#      {
-#        file = "/var/keys/secret_file";
-#        parentDirectory = {
-#          mode = "u=rwx,g=,o=";
-#        };
-#      }
-#    ];
+    #    files = [
+    #      "/etc/machine-id"
+    #      {
+    #        file = "/var/keys/secret_file";
+    #        parentDirectory = {
+    #          mode = "u=rwx,g=,o=";
+    #        };
+    #      }
+    #    ];
   };
   security.sudo.extraConfig = ''
     # rollback results in sudo lectures after each reboot
