c49ae14ef4e771c5b25bb406a28919720f3dccd2
Jan van de Locht <jan@vandelocht.uk>
2025-08-17 10:28:33 +0200

    fixed caching

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
       "flakes"
     ];
     extra-substituters = [
+      "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://devenv.cachix.org"
@@ -15,8 +16,10 @@
       "https://nix-gaming.cachix.org"
       "https://numtide.cachix.org"
       "https://tweag-nickel.cachix.org"
+      "https://cache.lan.vandelocht.uk/"
     ];
     trusted-substituters = [
+      "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://devenv.cachix.org"
@@ -25,8 +28,10 @@
       "https://nix-gaming.cachix.org"
       "https://numtide.cachix.org"
       "https://tweag-nickel.cachix.org"
+      "https://cache.lan.vandelocht.uk/"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     extra-trusted-public-keys = [
@@ -38,6 +43,7 @@
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "tweag-nickel.cachix.org-1:GIthuiK4LRgnW64ALYEoioVUQBWs0jexyoYVeLDBwRA="
+      "cache.lan.vandelocht.uk:uY5NlU5/9D6UTirWyuY8WTI+oEucGbSINnQfe6xrQbM="
     ];
   };
 
--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -88,15 +88,5 @@ in
     #   dates = "weekly";
     #   options = "--delete-older-than 30d";
     # };
-    settings.substituters = [
-      "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
-      "https://cache.lan.vandelocht.uk/"
-    ];
-    settings.trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.lan.vandelocht.uk:uY5NlU5/9D6UTirWyuY8WTI+oEucGbSINnQfe6xrQbM="
-    ];
   };
 }
