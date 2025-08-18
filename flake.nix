31c8f92aa324047f13a3f6c24d3ec035653559ec
Jan van de Locht <jan@vandelocht.uk>
2025-08-18 21:48:06 +0200

    Added host nixcache

--- flake.nix
+++ flake.nix
@@ -33,6 +33,14 @@
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "tweag-nickel.cachix.org-1:GIthuiK4LRgnW64ALYEoioVUQBWs0jexyoYVeLDBwRA="
+      "cache.lan.vandelocht.uk:uY5NlU5/9D6UTirWyuY8WTI+oEucGbSINnQfe6xrQbM="
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -178,6 +186,14 @@
             ];
           };
 
+          nixcache = mkNixosConfig {
+            system = "x86_64-linux";
+            hostname = "nixcache";
+            username = "jan";
+            extraModules = [
+            ];
+          };
+
           hetzner = mkNixosConfig {
             system = "x86_64-linux";
             hostname = "hetzner";
@@ -212,8 +228,7 @@
             system = "aarch64-linux";
             overlays = [
               nix-on-droid.overlays.default
-            ]
-            ++ commonOverlays;
+            ] ++ commonOverlays;
           };
           home-manager-path = home-manager.outPath;
           extraSpecialArgs = specialArgs;
---
+++ hosts/nixcache/configuration.nix
@@ -0,0 +1,142 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  zfsUtils = import ../../lib/zfs.nix { inherit lib pkgs config; };
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../common/configuration.nix
+    ./opt-in.nix
+  ];
+  locale.enable = true;
+  # gnome.enable = true;
+  nvidia.enable = false;
+  gaming.enable = false;
+  printing.enable = true;
+  sops-config.enable = true;
+  harmonia.enable = true;
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/jan/.setup";
+  };
+
+  boot = {
+    # Note this might jump back and forth as kernels are added or removed.
+    kernelPackages = zfsUtils.getLatestZfsKernel;
+    # Bootloader.
+    loader = {
+      grub = {
+        enable = true;
+        zfsSupport = true;
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+        mirroredBoots = [
+          {
+            devices = [ "nodev" ];
+            path = "/boot";
+          }
+        ];
+      };
+    };
+    initrd.postMountCommands = lib.mkAfter ''
+      zfs rollback -r rpool/local/root@blank
+    '';
+  };
+
+  networking = {
+    hostName = "nixcache"; # Define your hostname.
+    hostId = "3901c199";
+    networkmanager.enable = true;
+    firewall = {
+      allowPing = true;
+      extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
+    };
+  };
+
+  services = {
+    openssh.enable = true;
+    zfs.autoScrub.enable = true;
+    gvfs.enable = true;
+    udisks2.enable = true;
+    spice-vdagentd.enable = true;
+    spice-autorandr.enable = true;
+    spice-webdavd.enable = true;
+    qemuGuest.enable = true;
+    # backrest = {
+    #   enable = true;
+    #   bindAddress = "0.0.0.0";
+    #   port = 9898;
+    #   configSecret = "backrest-nixnas";
+    # };
+  };
+
+  # security.sudo.wheelNeedsPassword = false;
+  users = {
+    # groups.samba = {};
+    users = {
+      "jan" = {
+        isNormalUser = true;
+        hashedPasswordFile = config.sops.secrets.jan-nixnas.path;
+        home = "/home/jan";
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+          "users"
+        ];
+        linger = true;
+      };
+    };
+  };
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    trusted-users = [ "jan" ]; # Add your own username to the trusted list
+    auto-optimise-store = true;
+    max-jobs = "auto";
+    builders-use-substitutes = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages =
+    (with pkgs; [
+      # nixvim
+      spice
+      tmux
+    ])
+    ++ (with inputs; [
+    ]);
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
