fc81f36da44114289f9ce8442ca8f3efe558f61b
Jan van de Locht <jan@vandelocht.uk>
2025-08-18 23:48:37 +0200

    freecad: changed overlay to circumvent the necessity of rebuilding the package

--- overlays/freecad.nix
+++ overlays/freecad.nix
@@ -1,21 +1,18 @@
 inputs: final: prev: {
-  freecad = prev.freecad.overrideAttrs (
-    oldAttrs:
-    let
-      pyEnv = prev.python3.withPackages (ps: [
-        ps.requests
-        ps.gitpython
-      ]);
-    in
-    {
-      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.makeWrapper ];
-
-      postInstall =
-        (oldAttrs.postInstall or "")
-        + ''
-          wrapProgram $out/bin/FreeCAD \
-            --prefix PYTHONPATH : "${pyEnv}/${prev.python3.sitePackages}"
-        '';
-    }
-  );
+  freecad = prev.symlinkJoin {
+    name = "freecad-wrapped";
+    paths = [ prev.freecad ];
+    nativeBuildInputs = [ prev.makeWrapper ];
+    postBuild =
+      let
+        pyEnv = prev.python3.withPackages (ps: [
+          ps.requests
+          ps.gitpython
+        ]);
+      in
+      ''
+        wrapProgram $out/bin/FreeCAD \
+          --prefix PYTHONPATH : "${pyEnv}/${prev.python3.sitePackages}"
+      '';
+  };
 }
