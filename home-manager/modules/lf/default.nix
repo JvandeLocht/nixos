9dc8838e0a000229e5d4116b2b8225b084a30676
Jan van de Locht <jan@vandelocht.uk>
2025-11-07 08:20:07 +0100

    renamed dragon according to upstream change

--- home-manager/modules/lf/default.nix
+++ home-manager/modules/lf/default.nix
@@ -3,7 +3,8 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   options.lf = {
     enable = lib.mkEnableOption "Custom lf file manager configuration";
   };
@@ -14,7 +15,7 @@
     programs.lf = {
       enable = true;
       commands = {
-        dragon-out = ''%${pkgs.xdragon}/bin/xdragon -a -x "$fx"'';
+        dragon-out = ''%${pkgs.dragon-drop}/bin/xdragon -a -x "$fx"'';
         editor-open = "$$EDITOR $f";
         mkdir = ''
           ''${{
@@ -54,28 +55,30 @@
         ignorecase = true;
       };
 
-      extraConfig = let
-        previewer = pkgs.writeShellScriptBin "pv.sh" ''
-          file=$1
-          w=$2
-          h=$3
-          x=$4
-          y=$5
+      extraConfig =
+        let
+          previewer = pkgs.writeShellScriptBin "pv.sh" ''
+            file=$1
+            w=$2
+            h=$3
+            x=$4
+            y=$5
 
-          if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-              ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-              exit 1
-          fi
+            if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
+                ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+                exit 1
+            fi
 
-          ${pkgs.pistol}/bin/pistol "$file"
+            ${pkgs.pistol}/bin/pistol "$file"
+          '';
+          cleaner = pkgs.writeShellScriptBin "clean.sh" ''
+            ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+          '';
+        in
+        ''
+          set cleaner ${cleaner}/bin/clean.sh
+          set previewer ${previewer}/bin/pv.sh
         '';
-        cleaner = pkgs.writeShellScriptBin "clean.sh" ''
-          ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
-        '';
-      in ''
-        set cleaner ${cleaner}/bin/clean.sh
-        set previewer ${previewer}/bin/pv.sh
-      '';
     };
 
     # ... (other configurations)
