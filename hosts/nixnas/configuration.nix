247041586d53983ce68e824ae05ea487a090a182
Jan van de Locht <jan@vandelocht.uk>
2025-11-16 21:58:42 +0100

    Added config for jellyfin to nginx

--- hosts/nixnas/configuration.nix
+++ hosts/nixnas/configuration.nix
@@ -63,6 +63,7 @@
       extraDomainNames = [
         "vandelocht.uk"
         "img.lan.vandelocht.uk"
+        "jellyfin.lan.vandelocht.uk"
       ];
       dnsProvider = "cloudflare";
       environmentFile = config.sops.secrets."cloudflare-acme".path;
@@ -72,18 +73,33 @@
   services.nginx = {
     enable = true;
     recommendedTlsSettings = true;
-    virtualHosts."img.lan.vandelocht.uk" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/".extraConfig = ''
-        proxy_pass http://127.0.0.1:2283;
-        proxy_set_header Host $host;
-        proxy_redirect http:// https://;
-        proxy_http_version 1.1;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection $connection_upgrade;
-      '';
+    virtualHosts = {
+      "img.lan.vandelocht.uk" = {
+        useACMEHost = "vandelocht.uk";
+        forceSSL = true;
+        locations."/".extraConfig = ''
+          proxy_pass http://127.0.0.1:2283;
+          proxy_set_header Host $host;
+          proxy_redirect http:// https://;
+          proxy_http_version 1.1;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection $connection_upgrade;
+        '';
+      };
+      "jellyfin.lan.vandelocht.uk" = {
+        useACMEHost = "vandelocht.uk";
+        forceSSL = true;
+        locations."/".extraConfig = ''
+          proxy_pass http://127.0.0.1:8096;
+          proxy_set_header Host $host;
+          proxy_redirect http:// https://;
+          proxy_http_version 1.1;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection $connection_upgrade;
+        '';
+      };
     };
   };
 
